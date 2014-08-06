725727270960d4de544b2d94fb98bc10445ecbaa
Jayin Ton <tonjayin@gmail.com>
2014-08-06 12:33:29 +0800

    text msg step 1.

--- libs/entity/TextMsg.php
+++ libs/entity/TextMsg.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'Receive.php';
-
-class TextMsg extends Receive{
+class TextMsg extends Receive {
 	public $Content;
-    
 	
+	public function __construct($xml_string) {
+		parent::__construct ( $xml_string );
+		$obj = simplexml_load_string ( $xml_string );
+		$this->Content = self::getString ( $obj->Content );
+	}
 }
 
---
+++ test/test_textmsg.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'libs/entity/TextMsg.php';
+
+$xml_string = '
+ <xml>
+ <ToUserName>234234134</ToUserName>
+ <FromUserName>2342345</FromUserName> 
+ <CreateTime>1348831860</CreateTime>
+ <MsgType>text</MsgType>
+ <Content>Hi,I am Jayin</Content>
+ <MsgId>1234567890123456</MsgId>
+ </xml>';
+
+$msg = new TextMsg($xml_string);
+
+var_dump($msg);
