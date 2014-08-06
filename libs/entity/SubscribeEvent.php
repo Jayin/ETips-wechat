62cc015b520a931a5cb58f83d4dac7508dd8d01e
Jayin Ton <tonjayin@gmail.com>
2014-08-06 12:29:43 +0800

    int -> string

--- libs/entity/SubscribeEvent.php
+++ libs/entity/SubscribeEvent.php
@@ -5,8 +5,8 @@ class SubScribeEvent extends Event{
 	
 	public function  __construct($xml_string){
 		$obj = simplexml_load_string($xml_string);
-		$this->ToUserName = self::getInt($obj->ToUserName);
-		$this->FromUserName = self::getInt($obj->FromUserName);
+		$this->ToUserName = self::getString($obj->ToUserName);
+		$this->FromUserName = self::getString($obj->FromUserName);
 		$this->CreateTime = self::getInt($obj->CreateTime);
 		$this->MsgType = self::getString($obj->MsgType);
 		$this->Event = self::getString($obj->Event);
