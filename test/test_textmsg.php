6deab4a1512541710dcc88554d81925c0f32af9c
Jayin Ton <tonjayin@gmail.com>
2014-08-07 13:04:53 +0800

    add more test

--- test/test_textmsg.php
+++ test/test_textmsg.php
@@ -1,9 +1,20 @@
 <?php
-require_once 'libs/entity/TextMsg.php';
+use utils\HttpRequest;
+require_once 'libs/utils/HttpRequest.php';
 
-$xml_string = '
-<xml><ToUserName>234234134</ToUserName><FromUserName>2342345</FromUserName><CreateTime>1348831860</CreateTime><MsgType>text</MsgType><Content>Hi,I am Jayin</Content><MsgId>1234567890123456</MsgId></xml>';
+$req = new HttpRequest();
 
-$msg = new TextMsg($xml_string);
+$url='http://localhost/ETips-wechat/?signature=359432c62b290556eaa67cec6579490218f51f58&timestamp=1407324357&nonce=298696280';
+$xml_data = '
+<xml>
+<ToUserName>234234134</ToUserName>
+<FromUserName>2342345</FromUserName>
+<CreateTime>1348831860</CreateTime>
+<MsgType>text</MsgType>
+<Content>Hi,I am Jayin</Content>
+<MsgId>1234567890123456</MsgId>
+</xml>';
 
-var_dump($msg);
+$res = $req->post($url, $xml_data);
+
+var_dump($res);
--- test_image.php
+++ test_image.php
@@ -2,24 +2,19 @@
 use utils\HttpRequest;
 require_once 'libs/utils/HttpRequest.php';
 
-
-$req = new  HttpRequest();
-
-// $res = $req->get('http://www.baidu.com');
-// var_dump($res);
+$req = new HttpRequest();
 
 $url='http://localhost/ETips-wechat/?signature=359432c62b290556eaa67cec6579490218f51f58&timestamp=1407324357&nonce=298696280';
-$xml_data = '
- <xml>
+$xml_data =' <xml>
  <ToUserName>273942569</ToUserName>
- <FromUserName>383425941</FromUserName> 
+ <FromUserName>383425941</FromUserName>
  <CreateTime>1348831860</CreateTime>
- <MsgType>text</MsgType>
- <Content>Hellow Wechat</Content>
+ <MsgType>image</MsgType>
+ <PicUrl>https://avatars2.githubusercontent.com/u/2763894?v=2&s=40</PicUrl>
+ <MediaId>1231235</MediaId>
  <MsgId>1234567890123456</MsgId>
  </xml>';
 
-$req = new  HttpRequest();
 $res = $req->post($url, $xml_data);
 
 var_dump($res);
