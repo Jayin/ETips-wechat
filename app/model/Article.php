d599f32b13ea9812da5ed879bd74b0d50bc287f2
Jayin Ton <tonjayin@gmail.com>
2014-08-07 23:20:55 +0800

    web app init

---
+++ app/model/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace model;
+
+class Article {
+	/** 文章唯一id*/
+	public $Id;
+	/**图文消息标题 */
+	public $Title;
+	/** 图文消息描述*/
+	public $Description;
+	/**图片链接，支持JPG、PNG格式，较好的效果为大图360*200，小图200*200 */
+	public $PicUrl;
+	/** 点击图文消息跳转链接*/
+	public $Url;
+	function __construct() {
+	}
+}
+
+?>
---
+++ app/model/User.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace model;
+
+class User {
+	public $openid;
+	public $name;
+	public $email;
+	public $phone;
+	public $password;
+	
+	function __construct() {
+	}
+}
+
+ 
