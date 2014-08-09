aada963b50419b06b6dfcc9998736269c916441c
Jayin Ton <tonjayin@gmail.com>
2014-08-09 16:31:14 +0800

    more config

--- app/config.php
+++ app/config.php
@@ -1,24 +1,32 @@
 <?php
+/**
+Application Config
+ */
 $dev = true;
-$config = array(
-	//public config
+$config = array (
+		'view_folder' => __DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR,
+		'controller_folder' => __DIR__ . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR 
 );
 
 if ($dev) {
-	//dev 
-	ini_set("display_errors",'on');
-	error_reporting(E_ALL);
+	// dev
+	ini_set ( "display_errors", 'on' );
+	error_reporting ( E_ALL );
+	// DB config
+	$config ['USERNAME'] = 'root';
+	$config ['PSW'] = 'root';
+	$config ['PORT'] = '3306';
+	$config ['DBNAME'] = 'etips_wechat';
+	$config['App_Server'] = 'http://localhost/ETips-wechat/app/';
 	
-	$config['USERNAME'] = 'root';
-	$config['PSW'] = 'root';
-	$config['PORT'] = '3306';
-	$config['DBNAME'] = 'etips_wechat';
 	return $config;
 } else {
-	//product
-	$config['USERNAME'] = SAE_MYSQL_USER;
-	$config['PSW'] = SAE_MYSQL_PASS;
-	$config['PORT'] = SAE_MYSQL_PORT;
-	$config['DBNAME'] = SAE_MYSQL_DB;
+	// product
+	// DB config
+	$config ['USERNAME'] = SAE_MYSQL_USER;
+	$config ['PSW'] = SAE_MYSQL_PASS;
+	$config ['PORT'] = SAE_MYSQL_PORT;
+	$config ['DBNAME'] = SAE_MYSQL_DB;
+	$config['App_Server'] = 'http://etips.sinaapp.com/app/';
 	return $config;
 }
