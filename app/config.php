441da9b45ea2f76ad983bbb333e23d35b7530eab
Jayin Ton <tonjayin@gmail.com>
2014-08-10 21:17:15 +0800

    change the template file & php -> html

--- app/config.php
+++ app/config.php
@@ -4,10 +4,10 @@ Application Config
  */
 $dev = true;
 $config = array (
-		'view_folder' => __DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR,
+		'view_folder' => __DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . template . DIRECTORY_SEPARATOR,
 		'controller_folder' => __DIR__ . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR 
 );
-//display errors 
+// display errors
 ini_set ( "display_errors", 'on' );
 error_reporting ( E_ALL );
 
@@ -18,7 +18,7 @@ if ($dev) {
 	$config ['PSW'] = 'root';
 	$config ['PORT'] = '3306';
 	$config ['DBNAME'] = 'etips_wechat';
-	$config['App_Server'] = 'http://localhost/ETips-wechat/app/';
+	$config ['App_Server'] = 'http://localhost/ETips-wechat/app/';
 	
 	return $config;
 } else {
@@ -28,6 +28,6 @@ if ($dev) {
 	$config ['PSW'] = SAE_MYSQL_PASS;
 	$config ['PORT'] = SAE_MYSQL_PORT;
 	$config ['DBNAME'] = SAE_MYSQL_DB;
-	$config['App_Server'] = 'http://etips.sinaapp.com/app/';
+	$config ['App_Server'] = 'http://etips.sinaapp.com/app/';
 	return $config;
 }
