0e57ffeb8c2cfe49e9f7af083dd37854ccbc6781
Jayin Ton <tonjayin@gmail.com>
2014-08-14 23:07:40 +0800

    ~

--- app/controller/admin/manage.php
+++ app/controller/admin/manage.php
@@ -1,12 +1,15 @@
 <?php
-require_once '__init__.php';
+require_once '../common/__init__.php';
 
+//
 $app->get ( function ($app, $req, $res) {
+	$res->render('admin/manage.html');
 	
-	echo "Home Page";
 } );
 
 $app->post ( function ($app, $req, $res) {
 } );
 
 $app->work();
+	
+
